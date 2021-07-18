097fcd563fb3850a4057d6723e790cf964c62077
jordibuges <jordi.buges@gmail.com>
2021-07-18 12:58:30 +0200

    change to flutter fragment activity

--- android/src/main/kotlin/flutter/moum/hardware_buttons/EmptyActivityLifecycleCallbacks.kt
+++ android/src/main/kotlin/flutter/moum/hardware_buttons/EmptyActivityLifecycleCallbacks.kt
@@ -2,21 +2,22 @@ package flutter.moum.hardware_buttons
 
 import android.app.Activity
 import android.app.Application
+import android.app.FragmentActivity
 import android.os.Bundle
 
 
 open class EmptyActivityLifecycleCallbacks: Application.ActivityLifecycleCallbacks {
-    override fun onActivityPaused(activity: Activity?) { }
+    override fun onActivityPaused(activity: FragmentActivity?) { }
 
-    override fun onActivityResumed(activity: Activity?) { }
+    override fun onActivityResumed(activity: FragmentActivity?) { }
 
-    override fun onActivityStarted(activity: Activity?) { }
+    override fun onActivityStarted(activity: FragmentActivity?) { }
 
-    override fun onActivityDestroyed(activity: Activity?) { }
+    override fun onActivityDestroyed(activity: FragmentActivity?) { }
 
-    override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) { }
+    override fun onActivitySaveInstanceState(activity: FragmentActivity?, outState: Bundle?) { }
 
-    override fun onActivityStopped(activity: Activity?) { }
+    override fun onActivityStopped(activity: FragmentActivity?) { }
 
-    override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) { }
+    override fun onActivityCreated(activity: FragmentActivity?, savedInstanceState: Bundle?) { }
 }
